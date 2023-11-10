1c1cda202583a4fb975d6337411f378d867f5c57
Andy Boutte <andyboutte@gmail.com>
2023-11-09 20:46:57 -0600

    adding commit for demo

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ export const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello, world!');
+  res.send('Hello, world! this is a demo');
 });
 
 export const server = app.listen(port, () => {
