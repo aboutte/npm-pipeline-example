0f306336e30637bf7fc33588eb5b208c18fb1b0c
Andy Boutte <andyboutte@gmail.com>
2024-02-15 13:16:08 -0600

    up

--- iac/terraform/admin-tenant/main.tf
+++ iac/terraform/admin-tenant/main.tf
@@ -1,17 +1,7 @@
-locals {
-  plan_id     = var.infra_name
-  tenant_name = terraform.workspace
-  region = data.duplocloud_infrastructure.infra.region
-}
-
-data "duplocloud_infrastructure" "infra" {
-  infra_name = var.infra_name
-}
 
 resource "duplocloud_tenant" "tenant" {
   account_name   = local.tenant_name
   plan_id        = local.plan_id
-  allow_deletion = true
 }
 
 resource "duplocloud_tenant_config" "tenant-config" {
