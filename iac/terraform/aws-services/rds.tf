6318d4710fd685d6a5ef763d9ca649ed7d92c446
Andy Boutte <andyboutte@gmail.com>
2024-02-15 15:03:44 -0600

    up

--- iac/terraform/aws-services/rds.tf
+++ iac/terraform/aws-services/rds.tf
@@ -6,12 +6,12 @@ resource "random_password" "password" {
 
 resource "duplocloud_rds_instance" "db" {
   tenant_id      = local.tenant_id
-  name           = "app"
+  name           = "${local.tenant_id}-app"
   engine         = 0                          
   engine_version = "8.0.32"
   size           = "db.t3.medium"
 
-  master_username = "app-admin"
+  master_username = "appadmin"
   master_password = random_password.password.result
 
   encrypt_storage = true
