eeeeca3969ac8ac623fdbe6db54d1c25e81bbd0e
Andy Boutte <andyboutte@gmail.com>
2024-02-15 15:30:44 -0600

    up

--- iac/terraform/aws-services/ecr.tf
+++ iac/terraform/aws-services/ecr.tf
@@ -3,5 +3,5 @@ resource "duplocloud_aws_ecr_repository" "frontend" {
   tenant_id                 = local.tenant_id
   name                      = "frontend"
   enable_scan_image_on_push = true
-  enable_tag_immutability   = true
+  enable_tag_immutability   = false
 }
