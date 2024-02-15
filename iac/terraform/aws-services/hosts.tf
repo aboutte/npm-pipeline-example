536b7800b3266e498cb9e5dcd44fc6a9a268f691
Andy Boutte <andyboutte@gmail.com>
2024-02-15 10:40:50 -0600

    aws services

--- iac/terraform/aws-services/hosts.tf
+++ iac/terraform/aws-services/hosts.tf
@@ -4,33 +4,7 @@ data "duplocloud_native_host_image" "ami" {
   is_kubernetes = true
 }
 
-# in non-prod environments we will use a single instance, this will allow us to easily use a scheduler to shutdown the instance durring off hours
-resource "duplocloud_aws_host" "hosts" {
-
-  count = terraform.workspace == "ask-prod" ? 0 : 1
-
-  tenant_id           = local.tenant_id
-  friendly_name       = "host"
-  image_id            = data.duplocloud_native_host_image.ami.image_id
-  capacity            = var.asg_instance_type
-  agent_platform      = 7
-  zone                = 1
-  is_minion           = true
-  is_ebs_optimized    = false
-  encrypt_disk        = false
-  allocated_public_ip = false
-  cloud               = 0
-  keypair_type        = 2
-
-  metadata {
-    key   = "OsDiskSize"
-    value = var.asg_os_disk_size
-  }
-}
-
 module "nodegroup" {
-
-  count = terraform.workspace == "ask-prod" ? 1 : 0
 
   source             = "duplocloud/components/duplocloud//modules/eks-nodes"
   version            = "0.0.5"
--- iac/terraform/aws-services/outputs.tf
+++ iac/terraform/aws-services/outputs.tf
@@ -8,21 +8,6 @@ output "s3_search_arn" {
   description = "The ARN of the S3 bucket."
 }
 
-output "sqs_queue_fullname" {
-  value       = duplocloud_aws_sqs_queue.sqs_queue.fullname
-  description = "Name of the SQS queue"
-}
-
-output "sqs_queue_arn" {
-  value       = duplocloud_aws_sqs_queue.sqs_queue.arn
-  description = "ARN of the SQS queue"
-}
-
-output "sqs_queue_url" {
-  value       = duplocloud_aws_sqs_queue.sqs_queue.url
-  description = "URL of the SQS queue"
-}
-
 output "region" {
   value       = local.region
   description = "The AWS Region for the Tenant"
