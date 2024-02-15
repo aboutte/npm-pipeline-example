65b46f2e4e20500443b76218bb8d17abd13de8df
Andy Boutte <andyboutte@gmail.com>
2024-02-15 11:28:35 -0600

    up

--- iac/terraform/app/svc-frontend.tf
+++ iac/terraform/app/svc-frontend.tf
@@ -1,3 +1,4 @@
+
 resource "duplocloud_duplo_service" "frontend" {
   tenant_id                            = local.tenant_id
   name                                 = "frontend"
@@ -25,16 +26,16 @@ resource "duplocloud_duplo_service" "frontend" {
   }
 }
 
-resource "duplocloud_duplo_service_lbconfigs" "frontend_config" {
-  tenant_id                   = duplocloud_duplo_service.frontend.tenant_id
-  replication_controller_name = duplocloud_duplo_service.frontend.name
-  lbconfigs {
-    lb_type          = 7
-    is_native        = false
-    is_internal      = false
-    port             = 3000
-    external_port    = 3000
-    protocol         = "http"
-    health_check_url = "/"
-  }
-}
+# resource "duplocloud_duplo_service_lbconfigs" "frontend_config" {
+#   tenant_id                   = duplocloud_duplo_service.frontend.tenant_id
+#   replication_controller_name = duplocloud_duplo_service.frontend.name
+#   lbconfigs {
+#     lb_type          = 7
+#     is_native        = false
+#     is_internal      = false
+#     port             = 3000
+#     external_port    = 443
+#     protocol         = "http"
+#     health_check_url = "/"
+#   }
+# }
