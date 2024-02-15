f34a6e0e47b3ef269808d0b0176affdaf7a19d4e
Andy Boutte <andyboutte@gmail.com>
2024-02-15 13:36:22 -0600

    uop

--- iac/terraform/app/svc-frontend.tf
+++ iac/terraform/app/svc-frontend.tf
@@ -5,6 +5,7 @@ resource "duplocloud_duplo_service" "frontend" {
   replicas                             = var.replica_count
   lb_synced_deployment                 = false
   cloud_creds_from_k8s_service_account = false
+  http_to_https_redirect = true
   is_daemonset                         = false
   is_unique_k8s_node_required = true
   agent_platform                       = 7
@@ -26,6 +27,9 @@ resource "duplocloud_duplo_service" "frontend" {
   }
 }
 
+
+
+
 resource "duplocloud_duplo_service_lbconfigs" "frontend_config" {
   tenant_id                   = duplocloud_duplo_service.frontend.tenant_id
   replication_controller_name = duplocloud_duplo_service.frontend.name
@@ -38,6 +42,43 @@ resource "duplocloud_duplo_service_lbconfigs" "frontend_config" {
     external_port    = 443
     protocol         = "http"
     health_check_url = "/"
+  }
+}
+
+resource "duplocloud_aws_target_group_attributes" "askduplo_dev_listener_443_tg_attributes" {
+  tenant_id        = local.tenant_id
+  target_group_arn = duplocloud_aws_load_balancer_listener.askduplo_dev_listener_443.target_group_arn
+  attribute {
+    key   = "target_group_health.unhealthy_state_routing.minimum_healthy_targets.count"
+    value = "1"
+  }
+
+
+
+
+  attribute {
+    key   = "stickiness.lb_cookie.duration_seconds"
+    value = "86400"
+  }
+  attribute {
+    key   = "slow_start.duration_seconds"
+    value = "0"
+  }
+  attribute {
+    key   = "stickiness.app_cookie.duration_seconds"
+    value = "86400"
+  }
+  attribute {
+    key   = "target_group_health.dns_failover.minimum_healthy_targets.percentage"
+    value = "off"
+  }
+  attribute {
+    key   = "load_balancing.cross_zone.enabled"
+    value = "use_load_balancer_configuration"
+  }
+  attribute {
+    key   = "load_balancing.algorithm.type"
+    value = "round_robin"
   }
 }
 
