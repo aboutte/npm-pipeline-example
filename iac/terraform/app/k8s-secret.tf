9ea8cbc290f0974b5527551580edbf909c410116
Andy Boutte <andyboutte@gmail.com>
2024-02-16 10:39:40 -0600

    u

--- iac/terraform/app/k8s-secret.tf
+++ iac/terraform/app/k8s-secret.tf
@@ -1,14 +1,16 @@
 
-resource "duplocloud_k8_secret" "secret" {
-  tenant_id   = local.tenant_id
-  secret_name = "database"
-  secret_type = "Opaque"
-  secret_data = jsonencode({
-    "db_url" : local.db_url
-    "db_username" : local.db_username
-    "db_password" : local.db_password
-    }
-  )
+# Example using AWS SecretsManager secret that can be used by k8s (via k8s secret provider) and Lambda
+resource "duplocloud_tenant_secret" "database" {
+  tenant_id = local.tenant_id
+
+  # The full name will be:  duploservices-myapp-myjson
+  name_suffix = "database"
+
+  data = jsonencode({
+    "DB_URL" : local.db_url
+    "DB_USERNAME" : local.db_username
+    "DB_PASSWORD" : local.db_password
+    })
 }
 
 resource "duplocloud_k8_secret" "integration" {
@@ -21,11 +23,48 @@ resource "duplocloud_k8_secret" "integration" {
   )
 }
 
-resource "duplocloud_tenant_secret" "mysecret2" {
-  tenant_id = local.tenant_id
+resource "duplocloud_k8_secret_provider_class" "spc" {
+  tenant_id       = local.tenant_id
+  name            = "database"
+  secret_provider = "aws"
 
-  # The full name will be:  duploservices-myapp-myjson
-  name_suffix = "myjson"
+  secret_object {
+    name = "database"
+    type = "Opaque"
+    data {
+      key         = "DB_URL"
+      object_name = "DB_URL"
+    }
+    data {
+      key         = "DB_USERNAME"
+      object_name = "DB_USERNAME"
+    }
+    data {
+      key         = "DB_PASSWORD"
+      object_name = "DB_PASSWORD"
+    }
+  }
 
-  data = jsonencode({ foo = "bar" })
+  parameters = yamlencode(
+    [
+      {
+        "objectName" : "duploservices-dev02-database",
+        "objectType" : "secretsmanager",
+        "jmesPath" : [
+          {
+            "path" : "DB_URL",
+            "objectAlias" : "DB_URL"
+          },
+          {
+            "path" : "DB_USERNAME",
+            "objectAlias" : "DB_USERNAME"
+          },
+          {
+            "path" : "DB_PASSWORD",
+            "objectAlias" : "DB_PASSWORD"
+          }
+        ]
+      }
+    ]
+  )
 }
