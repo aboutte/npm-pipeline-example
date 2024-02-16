31e9aadd5d3c91bd3492e155c4f07358d9b90700
Andy Boutte <andyboutte@gmail.com>
2024-02-16 09:16:22 -0600

    up

--- iac/terraform/app/lambda.tf
+++ iac/terraform/app/lambda.tf
@@ -6,12 +6,8 @@ resource "duplocloud_aws_lambda_function" "app" {
   description = "Sample function that shows using AWS Secrets Manager"
 
   package_type = "Image"
-  image_uri    = "813590939111.dkr.ecr.us-west-2.amazonaws.com/frontend:lambda"
+  image_uri    = "813590939111.dkr.ecr.us-west-2.amazonaws.com/frontend:lambda2"
 
-  image_config {
-    command           = ["echo", "hello world", "DB=$(aws --region us-west-2 secretsmanager get-secret-value --secret-id duploservices-app01-database)", "echo $DB"]
-    working_directory = "/"
-  }
 
   tracing_config {
     mode = "PassThrough"
