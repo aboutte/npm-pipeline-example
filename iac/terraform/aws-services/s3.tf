9c1da712b72399107b1aea193270d8e2addd15ea
Andy Boutte <andyboutte@gmail.com>
2024-02-18 13:59:30 -0600

    up

--- iac/terraform/aws-services/s3.tf
+++ iac/terraform/aws-services/s3.tf
@@ -1,7 +1,7 @@
 
-resource "duplocloud_s3_bucket" "search" {
+resource "duplocloud_s3_bucket" "docs" {
   tenant_id           = local.tenant_id
-  name                = "search"
+  name                = "docs"
   allow_public_access = var.s3_search_allow_public_access
   enable_access_logs  = var.s3_search_enable_access_logs
   enable_versioning   = var.s3_search_enable_versioning
